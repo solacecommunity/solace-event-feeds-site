89fa7163592ed9db6702bd80a9fe213a42c6addd
gvensan <4477169+gvensan@users.noreply.github.com>
2024-05-23 17:23:30 +0530

    Event feeds site

---
+++ feedrunner/js/feed.js
@@ -0,0 +1,197 @@
+class IFeed {
+  feedName = undefined;
+  feed = {};
+
+  // CONSTRUCTOR
+
+  /**
+   * @param {string} feedName - event feed name
+   */
+  constructor (feedName) {
+    this.feedName = feedName;
+  }
+
+  /**
+   * Initialize feed: Handle all initialization operations
+   */
+  initialize () {
+    return false; // implementation should define the initialization
+  }
+
+  /**
+   * @param {string} param
+   */
+  setFeedParam (param, value) {
+    this.feed[param] = value;
+  }
+
+  /**
+   * @param {string} param
+   * 
+   * @return {string}
+   */
+  getFeedParam (param) {
+    return this.feed[param]
+  }
+
+
+  /**
+   * Get Feed Info
+   * 
+   * @return object, an object with feed information
+   */
+  getInfo () {
+    return undefined;   // implementation should define the logic
+  }
+
+  /**
+   * Get Send Messages
+   * 
+   * @return List<string>, a list of send messages
+   */
+  getSendMessages () {
+    return [];
+  }
+
+  /**
+   * Get Receive Messages
+   * 
+   * @return List<string>, a list of receive messages
+   */
+  getReceiveMessages () {
+    return [];
+  }
+
+  // HELPER FUNCTIONS
+  /**
+   * @return {string}
+   */
+  printFeed () {
+    return JSON.stringify( this, null, 2);
+  }
+}
+
+
+class Feed extends IFeed {
+  constructor(gitUrl) {
+    super(gitUrl);
+  }
+
+  async initialize () {
+    if (!this.feedName) {
+      return ({
+        status: false,
+        message: "Feed name missing!"
+      })
+    }
+    var info = await getGitFile(communityUserName, communityRepoName, `${feedName}/feedinfo.json`);
+    this.setFeedParam("info", info);
+    var rules = await getGitFile(communityUserName, communityRepoName, `${feedName}/feedrules.json`);
+    this.setFeedParam("rules", rules);
+    var schemas = await getGitFile(communityUserName, communityRepoName, `${feedName}/feedschemas.json`);
+    this.setFeedParam("schemas", schemas);
+    var analysis = await getGitFile(communityUserName, communityRepoName, `${feedName}/analysis.json`);
+    this.setFeedParam("analysis", analysis);
+  }
+
+  getInfo () {
+    var info = this.getFeedParam("info");
+    if (info) return info;
+
+    console.log(info);
+    
+    var analysis = this.getFeedParam("analysis");
+    if (!analysis) {
+      return ({
+        status: false,
+        message: "Feed not initialized!"
+      })
+    }
+
+    info = {
+      version: analysis.info.version,
+      asyncApiFile: analysis.fileName,
+      asyncApiVersion: analysis.version,
+      appTitle: info.name,
+      appDescription: analysis.info.description,
+      appState: analysis.info["x-ep-state-name"],
+      appDomainName: analysis.info["x-ep-application-domain-name"],
+    }
+
+    return info;
+  }
+
+  getReceiveMessages() {
+    var messages = [];
+    let rules = this.getFeedParam('rules');
+    let analysis = this.getFeedParam('analysis');
+    if (analysis && analysis.messages) {
+      var msgNames = Object.keys(analysis.messages);
+      msgNames.forEach(msg => {
+        if (analysis.messages[msg].send?.length) {
+          for (var i=0; i<analysis.messages[msg].send.length; i++) {
+            var data = {
+              messageName: msg,
+              description: analysis.messages[msg].send[i].message.description,
+              topicName: analysis.messages[msg].send[i].topicName,
+              hasPayload: analysis.messages[msg].hasPayload,
+              schema: analysis.messages[msg].schema,
+            };
+
+            if (rules) {
+              var rule = rules.find(el => el.messageName === msg);
+              data.count = rule ? rule.publishSettings.count : 20;
+              data.interval = rule ? rule.publishSettings.interval : 1;
+              data.delay = rule ? rule.publishSettings.delay : 0;
+            }
+            
+            messages.push(data);
+          }
+        }
+      })      
+    }
+
+    return messages;
+  }
+}
+
+function printHello(str) {
+  console.log('Hello', str);
+}
+
+async function getFeed(feedName) {
+  var feed = new Feed(feedName);
+  await feed.initialize();
+  return feed;
+}
+
+async function readFile (path) {
+  try {
+    // const config = fs.readFileSync(path, 'utf-8')
+    const config = await fetch(path, {mode: 'no-cors'})
+                            .then(d => d.json());
+    return config;
+  } catch (error) {
+    var errorMessage = `'read file failed: ${error.toString()} (${error.cause?.message} ? [${error.cause?.message}] : ''`;
+    return {
+      status: false,
+      message: errorMessage
+    }
+  }
+}
+
+async function getGitFile (owner, repo, path) { 
+  let data = await fetch (
+    `https://api.github.com/repos/${owner}/${repo}/contents/${path}`
+  )
+    .then (d => d.json ())
+    .then (d =>
+      fetch (
+        `https://api.github.com/repos/${owner}/${repo}/git/blobs/${d.sha}`
+      )
+    )
+    .then (d => d.json ())
+    .then (d => JSON.parse (atob (d.content)));
+
+  return data;
+}
---
+++ feeds/js/feeds-home.js
@@ -0,0 +1,293 @@
+const communityRepoUrl = 'https://github.com/solacecommunity/solace-event-feeds';
+const communityRepoRawUrl = 'https://raw.githubusercontent.com/solacecommunity';
+const communityUserName = 'solacecommunity';
+const communityRepoName = 'solace-event-feeds';
+const communityFeedsJson = 'EVENT_FEEDS.json';
+
+const openFeed = (feedName) => {
+  console.log('I am here', feedName);
+  const currLoc = $(location).attr('href');
+  const url = new URL(currLoc);
+  console.log('URL', `http://${url.host}/feed.html?feed=${feedName}`);
+  window.open(`http://${url.host}/feed.html?feed=${feedName}`, '_blank')
+}
+
+const getUser = async (url) => {
+  try {
+    const data = await fetch(url);
+    return data.json();
+  } catch (e) {
+    console.log("There was an error fetching the data: " + error)
+  }
+}
+
+const feeds = {};
+
+const buildLunrIndex = async (data) => {
+  feeds.data = data;
+  feeds.documents = [];
+  for (var i=0; i<data.length; i++) {
+    feeds.documents.push({
+      id: data[i].github,
+      ...data[i]
+    })
+  }
+
+  feeds.db = feeds.documents.reduce(function (acc, document) {
+    acc[document.id] = document
+    return acc
+  }, {})
+
+  feeds.idx = lunr(function () {
+    this.ref('id')
+    this.field('title', { boost: 10 })
+    this.field('description')
+    this.field('contributor')
+    this.field('name')
+    this.field('description')
+    this.field('domain')
+    this.field('tags')
+    feeds.documents.forEach(function (doc) {
+      this.add(doc)
+    }, this)
+  })
+}
+
+const filterByDomain = (evt) => {
+  var data = evt.dataset;
+  console.log(data)
+  var searchInput = document.querySelector('.js-shuffle-search');
+  searchInput.value = `domain:${data.domain}`;
+  searchInput.dispatchEvent(new window.Event('keyup', { bubbles: true }));
+}
+
+const filterByTag = (evt) => {
+  var data = evt.dataset;
+  console.log(data)
+  var searchInput = document.querySelector('.js-shuffle-search');
+  searchInput.value = `tags:${data.tag}`;
+  searchInput.dispatchEvent(new window.Event('keyup', { bubbles: true }));
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  $.ajaxSetup({
+      cache: false
+  })
+
+  // var feed = getFeed(feedName);
+  var data = await getGitFile(communityUserName, communityRepoName, communityFeedsJson)
+  console.log(data);
+
+  var parent = $('#feeds-grid');
+  for (var i=0; i<data.length; i++) {
+    var contributor = await getUser(`https://api.github.com/users/${data[i].github}`);
+
+    var feed = `
+    <div class="col-xl-6">
+      <div class="card info-card customers-card">
+        <div class="card-body">
+          <h5 class="card-title">${data[i].type}</h5>
+
+          <div class="d-flex align-items-center">
+            <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+              <img class="rounded-circle card-icon" src="${data[i].img}">
+            </div>
+            <div class="ps-3">
+              <h6><a href="${data[i].github}" target="_blank">${data[i].name}</a> </h6>
+              <div class="text-danger small pt-1 fw-bold">Contributor: <a href="https://github.com/${data[i].contributor}" target="_blank">${contributor.name}</a></div> 
+            </div>
+          </div>
+          <div class="text-muted small pt-2 ps-1">${data[i].description}</div>
+          <div class="d-flex align-center space-between">
+            <div class="ps-3">
+              <div class="text-muted small pt-2 ps-1 fw-bold">Domain: ${
+                data[i].domain.split(',').map(token => `<div data-domain="${token.trim()}" onclick="filterByDomain(this)" class="nav-link-button badge bg-dark">${token.trim()}</div>`).join(' ')
+              }</div>
+              <div class="text-muted small pt-2 ps-1 fw-bold ">Tags: ${
+                data[i].tags.split(',').map(token => `<div data-tag="${token.trim()}" onclick="filterByTag(this)" class="nav-link-button badge bg-dark">${token.trim()}</div>`).join(' ')
+              }</div>
+            </div>
+            <div class="ps-3 d-flex align-right">
+              <button type="button" class="btn btn-outline-primary" onclick="openFeed('${data[i].name}')">Open</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  `
+
+    parent.append( feed);
+
+  }
+
+  await buildLunrIndex(data);
+  
+  _handleSearchKeyup = async (evt) => {
+    const searchText = evt.target.value.toLowerCase();
+    var results = feeds.idx.search(searchText);
+    results.forEach(function (result) {
+      return feeds.db[result.ref]
+    })
+  
+    var parent = $('#feeds-grid');
+    parent.empty();
+    
+    var ids = results.map(r => r.ref);
+    for (var i=0; i<feeds.documents.length; i++) {
+      if (!ids.includes(feeds.documents[i].github))
+        continue;
+
+      var contributor = await getUser(`https://api.github.com/users/${data[i].contributor}`);
+  
+      var feed = `
+      <div class="col-xl-6">
+        <div class="card info-card customers-card">
+          <div class="card-body">
+            <h5 class="card-title">${data[i].type}</h5>
+  
+            <div class="d-flex align-items-center">
+              <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                <img class="rounded-circle card-icon" src="${data[i].img}">
+              </div>
+              <div class="ps-3">
+                <h6><a href="${data[i].github}" target="_blank">${data[i].name}</a> </h6>
+                <div class="text-danger small pt-1 fw-bold">Contributor: <a href="https://github.com/${data[i].contributor}" target="_blank">${contributor.name}</a></div> 
+              </div>
+            </div>
+            <div class="text-muted small pt-2 ps-1">${data[i].description}</div>
+            <div class="d-flex align-center space-between">
+              <div class="ps-3">
+                <div class="text-muted small pt-2 ps-1 fw-bold">Domain: ${
+                  data[i].domain.split(',').map(token => `<div data-domain="${token.trim()}" onclick="filterByDomain(this)" class="nav-link-button badge bg-dark">${token.trim()}</div>`).join(' ')
+                }</div>
+                <div class="text-muted small pt-2 ps-1 fw-bold ">Tags: ${
+                  data[i].tags.split(',').map(token => `<div data-tag="${token.trim()}" onclick="filterByTag(this)" class="nav-link-button badge bg-dark">${token.trim()}</div>`).join(' ')
+                }</div>
+              </div>
+              <div class="ps-3 d-flex align-right">
+                <button type="button" class="btn btn-outline-primary" onclick="openFeed('${data[i].name}')">Open</button>
+              </div>
+            </div>
+  
+          </div>
+        </div>
+      </div>
+    `
+  
+      parent.append( feed);
+    }
+  
+  
+  }
+
+  _handleSort = async (evt) => {
+    var ids = [];
+    const mode = evt.target.dataset.mode;
+    var data = await getFeeds();
+    if (mode === 'alphadown') {
+      let btnX = document.getElementById('sort-alpha-up')
+      btnX.classList.remove('active')
+      let btn = document.getElementById('sort-alpha-down')
+      if (btn.classList.contains('active')) {
+        btn.classList.remove('active')
+      } else {
+        btn.classList.add('active')
+        data.sort((a, b) => a.name.localeCompare(b.name));
+      }
+    } else if (mode === 'alphaup') {
+      let btnX = document.getElementById('sort-alpha-down')
+      btnX.classList.remove('active')
+      let btn = document.getElementById('sort-alpha-up')
+      if (btn.classList.contains('active')) {
+        btn.classList.remove('active')
+      } else {
+        btn.classList.add('active')
+        data.sort((a, b) => b.name.localeCompare(a.name));
+      }
+    } else if (mode === 'recent') {
+      let btnX = document.getElementById('sort-alpha-up')
+      btnX.classList.remove('active')
+      btnX = document.getElementById('sort-alpha-down')
+      btnX.classList.remove('active')
+      let btn = document.getElementById('sort-recent')
+      if (btn.classList.contains('active')) {
+        btn.classList.remove('active')
+      } else {
+        btn.classList.add('active')
+        data.sort((a, b) => new Date(a.lastModified) - new Date(b.lastModified));
+      }
+    }
+
+    await buildLunrIndex(data);
+  
+    const searchText = evt.target.value;
+    if (searchText) {
+      var results = feeds.idx.search(searchText.toLowerCase());
+      results.forEach(function (result) {
+        return feeds.db[result.ref]
+      })
+      ids = results.map(r => r.ref);
+    } else {
+      ids = feeds.documents.map(d => d.id);
+    }
+
+
+    var parent = $('#feeds-grid');
+    parent.empty();
+    
+    for (var i=0; i<data.length; i++) {
+      if (!ids.includes(data[i].github))
+        continue;
+
+      var contributor = await getUser(`https://api.github.com/users/${data[i].contributor}`);
+  
+      var feed = `
+      <div class="col-xl-6">
+        <div class="card info-card customers-card">
+          <div class="card-body">
+            <h5 class="card-title">${data[i].name}</h5>
+  
+            <div class="d-flex align-items-center">
+              <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                <img class="rounded-circle card-icon" src="${data[i].img}">
+              </div>
+              <div class="ps-3">
+                <h6><a href="${data[i].github}" target="_blank">${data[i].name}</a> </h6>
+                <div class="text-danger small pt-1 fw-bold">Contributor: <a href="https://github.com/${data[i].contributor}" target="_blank">${contributor.name}</a></div> 
+              </div>
+            </div>
+            <div class="text-muted small pt-2 ps-1">${data[i].description}</div>
+            <div class="d-flex align-center space-between">
+              <div class="ps-3">
+                <div class="text-muted small pt-2 ps-1 fw-bold">Domain: ${
+                  data[i].domain.split(',').map(token => `<div data-domain="${token.trim()}" onclick="filterByDomain(this)" class="nav-link-button badge bg-dark">${token.trim()}</div>`).join(' ')
+                }</div>
+                <div class="text-muted small pt-2 ps-1 fw-bold ">Tags: ${
+                  data[i].tags.split(',').map(token => `<div data-tag="${token.trim()}" onclick="filterByTag(this)" class="nav-link-button badge bg-dark">${token.trim()}</div>`).join(' ')
+                }</div>
+              </div>
+              <div class="ps-3 d-flex align-right">
+                <button type="button" class="btn btn-outline-primary" onclick="openFeed('${data[i].github}')">Open</button>
+              </div>
+            </div>
+  
+          </div>
+        </div>
+      </div>
+    `
+  
+      parent.append( feed);
+    }
+  }
+
+  // filtering
+  const searchInput = document.querySelector('.js-shuffle-search');
+  searchInput.addEventListener('keyup', _handleSearchKeyup);
+  searchInput.addEventListener('search', _handleSearchKeyup);
+
+  // sorting
+  const sortFeed = document.querySelectorAll('.sort-feed');
+  sortFeed.forEach(el => el.addEventListener('click', _handleSort));
+});
+
