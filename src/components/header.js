0356e618a93d362cb5063aabd022bc1effaa899b
Maryam Lim-Baig <maryam.lim-baig@solace.com>
2026-02-11 17:03:59 -0500

    Replaced old header with new header

--- src/components/header.js
+++ src/components/header.js
@@ -1,75 +1,18 @@
-import React from "react"
-import { Container, Navbar, Nav } from "react-bootstrap"
-import solaceDevLogo from "../images/solace-developers-logo-white.svg"
+import React, { useEffect } from "react"
 
-const Header = () => (
-  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-    <Container>
-      <Navbar.Brand
-        href="/"
-      >
-        <img
-          src={solaceDevLogo}
-          alt="Solace Developers Logo"
-          width="250px"
-        />
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse
-        id="responsive-navbar-nav"
-        className="justify-content-end"
-      >
-        <Nav className="mr-auto">
-          <Nav.Link
-            href="https://solace.com/try-it-now/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Getting Started
-          </Nav.Link>
-          <Nav.Link href="https://tutorials.solace.dev/" target="_blank" className="active">
-            Tutorials
-          </Nav.Link>
-          <Nav.Link
-            href="https://solace.community/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Community
-          </Nav.Link>
-          <Nav.Link
-            href="https://docs.solace.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Docs
-          </Nav.Link>
-          <Nav.Link
-            href="https://solace.com/downloads/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Downloads
-          </Nav.Link>
-          <Nav.Link
-            href="https://solace.com/learn/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn
-          </Nav.Link>
-        </Nav>
-        <a
-          href="https://console.solace.cloud/login"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="btnSmall"
-        >
-          Log In
-        </a>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-)
+const Header = () => {
+  useEffect(() => {
+    if (document.getElementById("solace-header-script")) return
+
+    const script = document.createElement("script")
+    script.id = "solace-header-script"
+    script.src = "https://devrelassets.z9.web.core.windows.net/header.js"
+    script.async = true
+
+    document.body.appendChild(script)
+  }, [])
+
+  return null
+}
 
 export default Header
